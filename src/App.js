982977e5fdf5b1adc1b4058fd3b3d8503a86f0f0
kristinaradeva1 <kriradeva@abv.bg>
2024-09-14 19:04:20 +0300

    Added a new to-do by using React State

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import React, {useState} from 'react'
 import './App.css';
 import TodoTable from './components/TodoTable';
 
 function App() {
 
-  const todos = [
+  const [todos, setTodos] = useState([
     {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
     {rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two'},
     {rowNumber: 3, rowDescription: 'Make dinner', rowAssigned: 'User One'},
     {rowNumber: 4, rowDescription: 'Charge phone battery', rowAssigned: 'User One'}
   ]
+)
 
   const addTodo = () => {
     if(todos.length > 0) {
@@ -17,8 +19,7 @@ function App() {
          rowDescription: 'New todo',
          rowAssigned: 'User Three'
         };
-        todos.push(newTodo);
-        console.log(todos);
+        setTodos(todos => [...todos, newTodo])
     }
   }
 
